93ae58fc9743bb521a8e00b40eca51c7c554f306
Grigorii Matiukhin <contact@gmatiukhin.site>
2024-09-29 02:16:04 +0300

    Revert "regenerate parser"
    This reverts commit 5ca48a2bbc10ed960ff0d47781bc144681ecc1a0.

--- bindings/rust/lib.rs
+++ bindings/rust/lib.rs
@@ -1,18 +1,13 @@
-//! This crate provides Kittyconf language support for the [tree-sitter][] parsing library.
+//! This crate provides kittyconf language support for the [tree-sitter][] parsing library.
 //!
 //! Typically, you will use the [language][language func] function to add this language to a
 //! tree-sitter [Parser][], and then use the parser to parse some code:
 //!
 //! ```
-//! let code = r#"
-//! "#;
+//! let code = "";
 //! let mut parser = tree_sitter::Parser::new();
-//! let language = tree_sitter_kittyconf::LANGUAGE;
-//! parser
-//!     .set_language(&language.into())
-//!     .expect("Error loading Kittyconf parser");
+//! parser.set_language(tree_sitter_kittyconf::language()).expect("Error loading kittyconf grammar");
 //! let tree = parser.parse(code, None).unwrap();
-//! assert!(!tree.root_node().has_error());
 //! ```
 //!
 //! [Language]: https://docs.rs/tree-sitter/*/tree_sitter/struct.Language.html
@@ -20,26 +15,30 @@
 //! [Parser]: https://docs.rs/tree-sitter/*/tree_sitter/struct.Parser.html
 //! [tree-sitter]: https://tree-sitter.github.io/
 
-use tree_sitter_language::LanguageFn;
+use tree_sitter::Language;
 
 extern "C" {
-    fn tree_sitter_kittyconf() -> *const ();
+    fn tree_sitter_kittyconf() -> Language;
 }
 
-/// The tree-sitter [`LanguageFn`] for this grammar.
-pub const LANGUAGE: LanguageFn = unsafe { LanguageFn::from_raw(tree_sitter_kittyconf) };
+/// Get the tree-sitter [Language][] for this grammar.
+///
+/// [Language]: https://docs.rs/tree-sitter/*/tree_sitter/struct.Language.html
+pub fn language() -> Language {
+    unsafe { tree_sitter_kittyconf() }
+}
 
 /// The content of the [`node-types.json`][] file for this grammar.
 ///
 /// [`node-types.json`]: https://tree-sitter.github.io/tree-sitter/using-parsers#static-node-types
-pub const NODE_TYPES: &str = include_str!("../../src/node-types.json");
+pub const NODE_TYPES: &'static str = include_str!("../../src/node-types.json");
 
-// NOTE: uncomment these to include any queries that this grammar contains:
+// Uncomment these to include any queries that this grammar contains
 
-// pub const HIGHLIGHTS_QUERY: &str = include_str!("../../queries/highlights.scm");
-// pub const INJECTIONS_QUERY: &str = include_str!("../../queries/injections.scm");
-// pub const LOCALS_QUERY: &str = include_str!("../../queries/locals.scm");
-// pub const TAGS_QUERY: &str = include_str!("../../queries/tags.scm");
+// pub const HIGHLIGHTS_QUERY: &'static str = include_str!("../../queries/highlights.scm");
+// pub const INJECTIONS_QUERY: &'static str = include_str!("../../queries/injections.scm");
+// pub const LOCALS_QUERY: &'static str = include_str!("../../queries/locals.scm");
+// pub const TAGS_QUERY: &'static str = include_str!("../../queries/tags.scm");
 
 #[cfg(test)]
 mod tests {
@@ -47,7 +46,7 @@ mod tests {
     fn test_can_load_grammar() {
         let mut parser = tree_sitter::Parser::new();
         parser
-            .set_language(&super::LANGUAGE.into())
-            .expect("Error loading Kittyconf parser");
+            .set_language(super::language())
+            .expect("Error loading kittyconf language");
     }
 }
